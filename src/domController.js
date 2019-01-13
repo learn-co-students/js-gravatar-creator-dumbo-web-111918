9ef481d4116946cc5d11f9712be020f81041384e
github username <patcarrasco@outlook.com>
2019-01-12 19:09:46 -0500

    Done.

--- src/domController.js
+++ src/domController.js
@@ -1,3 +1,42 @@
-function updateDOM() {
+function handleSubmit(e) {
 
+  if (e.target[0].value === "") {
+    clearGrav()
+    return false
+  }
+
+  e.preventDefault()
+
+  let check;
+
+  const iden = new Identicon(e.target[0].value)
+  // column 1 & 5
+  for (let i = 0; i < 15; i++) {
+    col1 = document.getElementById(`${i}-0`)
+    col5 = document.getElementById(`${i}-4`)
+
+    col2 = document.getElementById(`${i-5}-1`)
+    col4 = document.getElementById(`${i-5}-3`)
+
+    col3 = document.getElementById(`${i-10}-2`)
+
+    check = iden.hashed[i] % 2 === 0
+    if (!check) {
+      if (i < 5) {
+        col1.style.background = iden.color
+        col5.style.background = iden.color
+      } else if (i >= 5 && i < 10) {
+        col2.style.background = iden.color
+        col4.style.background = iden.color
+      } else if (i >= 10) {
+        col3.style.background = iden.color
+      }
+    }
+  }
+  return true
+}
+
+function clearGrav() {
+  let allSq = document.querySelectorAll("#identicon > span")
+  allSq.forEach(e => e.style.background = "")
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-function handleSubmit(e) {
-
-}
-
 document.addEventListener("DOMContentLoaded", () => {
   const form = document.getElementById("identicon-form")
   form.addEventListener("submit", handleSubmit)
+
+  const input = document.querySelector("#identicon-form > div > input")
+  input.addEventListener("keydown", clearGrav)
 })
